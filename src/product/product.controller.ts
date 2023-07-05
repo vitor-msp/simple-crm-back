be81857de52b8951f5cab6bad9a7564544b26043
Vitor Mateus <vitor7jan@gmail.com>
2023-07-05 10:05:22 -0300

    feat(product): added try catch in controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,49 +3,71 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
-import {
-  CreateProductInputDto,
-  DefaultProductOutputDto,
-  GetProductOutputDto,
-  PutProductInputDto,
-} from './product.dto';
+import { CreateProductInputDto, PutProductInputDto } from './product.dto';
+import { Response } from 'express';
 
 @Controller('/product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
-  async post(
-    @Body() input: CreateProductInputDto,
-  ): Promise<DefaultProductOutputDto> {
-    return this.productService.create(input);
+  async post(@Body() input: CreateProductInputDto, @Res() res: Response) {
+    try {
+      const output = await this.productService.create(input);
+      res.status(HttpStatus.CREATED).json(output);
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 
   @Get('/:id')
-  async get(@Param('id') id: string): Promise<GetProductOutputDto> {
-    return this.productService.get(id);
+  async get(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const output = await this.productService.get(id);
+      res.status(HttpStatus.OK).json(output);
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 
   @Get()
-  async getAll(): Promise<GetProductOutputDto[]> {
-    return this.productService.getAll();
+  async getAll(@Res() res: Response) {
+    try {
+      const output = await this.productService.getAll();
+      res.status(HttpStatus.OK).json(output);
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 
   @Put('/:id')
   async put(
     @Param('id') id: string,
     @Body() input: PutProductInputDto,
-  ): Promise<DefaultProductOutputDto> {
-    return this.productService.update(id, input);
+    @Res() res: Response,
+  ) {
+    try {
+      const output = await this.productService.update(id, input);
+      res.status(HttpStatus.OK).json(output);
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 
   @Delete('/:id')
-  async delete(@Param('id') id: string): Promise<DefaultProductOutputDto> {
-    return this.productService.delete(id);
+  async delete(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const output = await this.productService.delete(id);
+      res.status(HttpStatus.OK).json(output);
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).send();
+    }
   }
 }
