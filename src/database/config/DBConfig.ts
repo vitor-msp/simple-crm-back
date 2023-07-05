e67de9fd9d90657eac46e9078ea4ba77a2dbaa38
Vitor Mateus <vitor7jan@gmail.com>
2023-07-05 13:10:28 -0300

    chore(db): setted autoLoadEntities option in typeorm

--- src/database/config/DBConfig.ts
+++ src/database/config/DBConfig.ts
@@ -1,31 +1,25 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { ProductDB } from '../schema/ProductDB';
-import { CustomerDB } from '../schema/CustomerDB';
-import { BudgetDB } from '../schema/BudgetDB';
-import { BudgetItemDB } from '../schema/BudgetItemDB';
 
 export abstract class DBConfig {
   static getInfo(): TypeOrmModuleOptions {
-    const type = 'mysql';
-    const entities = [ProductDB, CustomerDB, BudgetDB, BudgetItemDB];
     const production: TypeOrmModuleOptions = {
-      type,
+      type: 'mysql',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities,
       synchronize: false,
+      autoLoadEntities: true,
     };
     const test: TypeOrmModuleOptions = {
-      type,
+      type: 'mysql',
       host: process.env.DB_HOST_TEST,
       port: +process.env.DB_PORT_TEST,
       username: process.env.DB_USER_TEST,
       password: process.env.DB_PASSWORD_TEST,
       database: process.env.DB_NAME_TEST,
-      entities,
+      autoLoadEntities: true,
       synchronize: true,
     };
     return process.env.PRODUCTION == 'true' ? production : test;
