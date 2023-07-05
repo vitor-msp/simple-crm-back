2517d7b3da13d6d64cc82781a4c6f49ec3e61202
Vitor Mateus <vitor7jan@gmail.com>
2023-07-05 20:28:07 -0300

    chore(nest): removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ ConfigModule.forRoot();
 
 import { DataSource } from 'typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
+import { ProductModule } from './modules/product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DBConfig } from './database/config/DBConfig';
-import { CustomerModule } from './customer/customer.module';
+import { CustomerModule } from './modules/customer/customer.module';
 import { BudgetModule } from './modules/budget/budget.module';
 
 @Module({
@@ -18,8 +16,6 @@ import { BudgetModule } from './modules/budget/budget.module';
     BudgetModule,
     TypeOrmModule.forRoot(DBConfig.getInfo()),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
